ca4af473b2d6b34f2bd91da31ea4a5278ed0eaab
Nirmani Abisheka <117269282+AbishekaN@users.noreply.github.com>
2024-03-20 15:23:59 +0530

    Create Appointment Page

---
+++ src/pages/AddAppointmentPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddAppointment from '../components/Forms/AddAppointment';
+
+
+const AddAppointmentPage = () => {
+    return (
+        <>
+            <AddAppointment />
+        </>
+    )
+}
+
+export default AddAppointmentPage
